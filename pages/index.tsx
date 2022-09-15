52fd86222730af7a811744b099d1a14488546acd
el7amza1 <elhamza19932020@gmail.com>
2022-09-15 17:48:38 +0200

    h

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
          <div className=" flex mt-5 items-center">
 
           <span className="absolute top-5 left-15 ml-4 h-full w-0.5 bg-gray-200 handl" aria-hidden="true"></span>
-        <div className=""> 
+        <div className="z-10 relative	"> 
           <img
             className="inline-block h-9 w-9 rounded-full"
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
