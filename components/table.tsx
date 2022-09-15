aee76a55d397d96d0062f21b7b2f235bff4ade56
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2022-09-15 17:43:29 +0200

    Add layout component

--- components/table.tsx
+++ components/table.tsx
@@ -41,7 +41,7 @@ export default function Table() {
                                                         name="weight"
                                                         type="number"
                                                         autoComplete="weight"
-                                                        value={person.reps}
+                                                        value={`${person.reps}`}
                                                         required
                                                         className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                                     />
@@ -54,7 +54,7 @@ export default function Table() {
                                                         name="reps"
                                                         type="number"
                                                         autoComplete="reps"
-                                                        value={person.reps}
+                                                        value={`${person.reps}`}
                                                         required
                                                         className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                                     />
