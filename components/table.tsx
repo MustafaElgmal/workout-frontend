9869c5f4fb4eba8307c014e62e861fac68b17cb1
el7amza1 <elhamza19932020@gmail.com>
2022-09-22 22:54:20 +0200

    post logs

--- components/table.tsx
+++ components/table.tsx
@@ -3,13 +3,16 @@ import { AppProps, RecType } from "../types"
 import React from "react"
 import { useState } from "react"
 import { getArrayOfSet } from "../utils/functions"
+import { useUser } from "@supabase/auth-helpers-react"
+import RowTable from "./rowTable"
 
 export default function Table({ workoutline }: AppProps) {
     const [recs, setRecs] = useState<RecType[]>([])
-
+    const { user } = useUser()
     useEffect(() => {
         getArrayOfSet(workoutline!, setRecs)
     }, []);
+    
     return (
         <div className=" w-1/2 md:w-full">
 
@@ -34,47 +37,7 @@ export default function Table({ workoutline }: AppProps) {
                                 </thead>
                                 <tbody className="bg-white">
                                     {recs.map((ele) => (
-                                        <tr key={ele.recSet} className='bg-gray-50'>
-                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                                                {ele.recSet}
-                                            </td>
-                                            <td className="whitespace-nowrap w-50 px-3 py-4 text-sm text-gray-500">
-                                                <input
-                                                    id="weight"
-                                                    name="weight"
-                                                    type="number"
-                                                    autoComplete="weight"
-                                                    value={`${ele.recWeights}`}
-                                                    required
-                                                    className="block w-full md:w:1/3 appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                                />
-                                            </td>
-                                            <td className="whitespace-nowrap w-50 px-3 py-4 text-sm text-gray-500">
-
-                                                <div className="flex items-center">
-                                                    <input
-                                                        id="reps"
-                                                        name="reps"
-                                                        type="number"
-                                                        autoComplete="reps"
-                                                        value={`${ele.recReps}`}
-                                                        required
-                                                        className="block w-full md:w:1/3 appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                                    />
-                                                    <div className="mx-5">
-                                                        <input
-                                                            id="candidates"
-                                                            aria-describedby="candidates-description"
-                                                            name="candidates"
-                                                            type="checkbox"
-                                                            className="h-4 w-4 rounded border-gray-600 text-indigo-600 focus:ring-black focus:text-white focus:bg-black checked:bg-black checked:text-white"
-                                                        />
-                                                    </div>
-
-                                                </div>
-                                            </td>
-
-                                        </tr>
+                                        <RowTable ele={ele} key={ele.recSet} workoutline={workoutline}/>
                                     ))}
                                 </tbody>
                             </table>
